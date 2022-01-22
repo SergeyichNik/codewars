01faefce81ef77d16721569ed23472d92a224528
SergeichNik <tyunev.nik89@gmail.com>
2022-01-22 14:03:04 +0300

    day2, ex6

--- script05.js
+++ script05.js
@@ -1,17 +1,27 @@
-const arr = ['bad', 'bad', 'bad', 'bad'];
+const arr = ['good', 'good', 'good', 'good', 'bad'];
+const arr1 = ['bad'];
+const arr2 = ['good', 'good', 'bad'];
 
 function well(x){
-    let reg = /good/gi;
-    let newArr = reg.exec(x);
-    if (newArr.length = 0) {
+    let newArr = [];
+    x.forEach(item => {
+        if (item == 'good') {
+            newArr.push(item)
+        }
+    })
+    if (newArr.length > 0) {
+        if (newArr.length > 2) {
+            return 'I smell a series!';
+        } else {
+            return 'Publish!';
+        }
+    } else {
         return 'Fail!';
     }
-    if (newArr.length > 2) {
-        return 'I smell a series!';
-    } else {
-        return 'Publish!';
-    }
+
 
 }
 
 console.log(well(arr));
+console.log(well(arr1));
+console.log(well(arr2));
