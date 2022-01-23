13fd725bcfa3336a17c38cde73cb863d507e89fa
SergeichNik <tyunev.nik89@gmail.com>
2022-01-23 23:08:26 +0300

    day3, ex12

---
+++ script12.js
@@ -0,0 +1,13 @@
+function repeatStr (n, s) {
+    return s.repeat(n)
+
+    // let arr = [];
+    // for (let i = 0; i < n; i++) {
+    //     arr.push(s);
+    // }
+    // return arr.join('');
+}
+
+
+
+console.log(repeatStr(3, "*"))
