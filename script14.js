329711934f976479ae87db949ccb5912383aca9c
SergeichNik <tyunev.nik89@gmail.com>
2022-01-25 22:52:49 +0300

    i forget n of ex

---
+++ script14.js
@@ -0,0 +1,36 @@
+function SubtractSum(n){
+    return 'apple';
+}
+}
+
+console.log(SubtractSum(325));
+
+function century(year) {
+    
+    return  Math.ceil(year / 100);
+  }
+
+function numberToString(num) {
+    return new String(num);
+}  
+
+
+console.log(numberToString(100))
+
+function otherAngle(a, b) {
+    return 180 - (a + b);
+  }
+
+function nthEven(n){
+    // let arr = [];
+    // for (let i = 0; i <= (n * 2); i++) {
+    //     if (i % 2 === 0) {
+    //         arr.push(i);
+    //     }
+        
+    // }   
+    // return arr[n - 1];
+    return (n * 2) - 2;
+}
+
+console.log(nthEven(100));
