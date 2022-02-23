d231cabd94b53cf5e3a185795fcb77d2d1be7175
SergeichNik <tyunev.nik89@gmail.com>
2022-02-24 00:07:55 +0300

    02.24

--- script16.js
+++ script16.js
@@ -12,10 +12,35 @@
 
 // console.log(expressionMatter(1, 1, 1))
 
-function opposite(num) {
-    return num * -1;
+// function opposite(num) {
+//     return num * -1;
+// }
+
+// function litres(time) {
+//     return Math.floor(time * 0.5);
+//   }
+
+// function greet() {
+//    return "hello world!";
+// }
+
+// console.log(typeof(greet))
+
+function remainder(n, m){
+    
+    if (m === 0 && n > 0) {
+        return NaN
+    }
+    if (n < m) {
+        return m % n
+    } else {
+        return n % m
+    }
+      
 }
 
-function litres(time) {
-    return Math.floor(time * 0.5);
-  }
+console.log(remainder(-178, 989))
+console.log(remainder(13, 72))
+console.log(remainder(0, -1))
+console.log(remainder(0, 1))
+console.log(remainder(-956, 0))
