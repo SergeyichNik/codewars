23301c2211cc7842b0d829e7ee9376acc5c163fc
SergeichNik <tyunev.nik89@gmail.com>
2022-03-11 23:11:49 +0300

    11.03.22

--- script17.js
+++ script17.js
@@ -1,11 +1,19 @@
-function symmetricPoint(p, q) {
-    return [2 * q[0] - p[0], 2 * q[1] - p[1]];
+// function symmetricPoint(p, q) {
+//     return [2 * q[0] - p[0], 2 * q[1] - p[1]];
+// }
+
+// console.log(symmetricPoint([0,0], [1,1]));
+
+// function oddCount(n){
+//    return Math.floor(n / 2)
+// }
+
+// console.log(oddCount(7))
+
+function usdcny(usd) {
+    return `${(usd * 6.75).toFixed(2)} Chinese Yuan`
 }
 
-console.log(symmetricPoint([0,0], [1,1]));
 
-function oddCount(n){
-   return Math.floor(n / 2)
-}
 
-console.log(oddCount(7))
+console.log(usdcny(8008))
